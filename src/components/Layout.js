879e70c8839add48f6238eba7a4f5b1bb15a3855
Samuel Tobias Roland Uyet <samu1493@stud.kea.dk>
2023-06-01 10:10:39 +0200

    favicon added

---
+++ src/components/Layout.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+export default function () {
+  return (
+    <>
+      <Head>
+        <link
+          rel="shortcut icon"
+          href="http://webdesam.dk/test/logoSmall.svg"
+        />
+      </Head>
+    </>
+  );
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,8 @@
 import "@/styles/globals.css";
 import { createContext, useState } from "react";
 import { Playfair_Display } from "next/font/google";
-import {StyledEngineProvider} from '@mui/material';
-
-
+import { StyledEngineProvider } from "@mui/material";
+import Layout from "@/components/Layout";
 
 const playfairDisplay = Playfair_Display({
   subsets: ["latin"],
@@ -16,12 +15,13 @@ export default function App({ Component, pageProps }) {
   const [bookingInformation, setBookingInformation] = useState({});
   return (
     <>
-<StyledEngineProvider injectFirst>
-      <BookingInformation.Provider value={[bookingInformation, setBookingInformation]}>
-        <main className={`${playfairDisplay.variable} font-serif `}>
-          <Component {...pageProps} />
-        </main>
-      </BookingInformation.Provider>
+      <StyledEngineProvider injectFirst>
+        <BookingInformation.Provider value={[bookingInformation, setBookingInformation]}>
+          <Layout />
+          <main className={`${playfairDisplay.variable} font-serif `}>
+            <Component {...pageProps} />
+          </main>
+        </BookingInformation.Provider>
       </StyledEngineProvider>
     </>
   );
