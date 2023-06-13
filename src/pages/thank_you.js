d9e12ad09ed82046360720c511c1b2879b04a2ef
jona08n9 <99641671+jona08n9@users.noreply.github.com>
2023-06-13 15:50:12 +0200

    Edits2

--- src/pages/thank_you.js
+++ src/pages/thank_you.js
@@ -10,16 +10,9 @@ export default function thankYou() {
       <main className="mx-1 mt-8 max-w-full rounded-sm bg-gradient-to-b from-color-opacity-20 to-color-opacity-10 px-8 pt-8 md:mx-auto md:max-w-2xl">
         <h2 className=" text-center">Thanks for ordering</h2>
         <h3>Order confirmed</h3>
-        <p>
-          Thank you for placing your order with us! We have sent an email confirmation to the address you provided. Please check
-          your inbox for further details. To view the program and additional information, click the button below. We hope you
-          enjoy the program and have a wonderful experience with us!
-        </p>
+        <p>Thank you for placing your order with us! We have sent an email confirmation to the address you provided. Please check your inbox for further details. To view the program and additional information, click the button below. We hope you enjoy the program and have a wonderful experience with us!</p>
         <div className=" mb-16 mt-10 flex justify-center">
-          <Button
-            className=" mb-10 h-10 gap-5 place-self-center rounded-none border-2 border-solid border-color-yellow px-6 font-sans font-semibold text-color-yellow hover:bg-color-yellow hover:text-color-black "
-            onClick={() => router.push("https://festival-app-six.vercel.app/")}
-          >
+          <Button className=" mb-10 h-10 gap-5 place-self-center rounded-none border-2 border-solid border-color-yellow px-6 font-sans font-semibold text-color-yellow hover:bg-color-yellow hover:text-color-black " onClick={() => router.push("https://jonas-festival-app.vercel.app/program")}>
             <span className="pt-1"> See program</span> <span className="material-symbols-outlined">arrow_forward</span>
           </Button>
         </div>
